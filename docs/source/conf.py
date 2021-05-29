04615a438fa7e111a392fa2c0436f7a0ac829e9c
Robin Thibaut <robin.thibaut@ugent.be>
2021-05-29 16:30:33 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@ import sys
 # sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'skbel'
@@ -32,7 +31,7 @@ release = '1.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
