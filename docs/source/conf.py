ee0a133505a2314509b0c5745b88409bbc4391fa
Robin Thibaut <robin.thibaut@ugent.be>
2021-05-29 16:35:41 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
+master_doc = 'index'
+
 # -- Project information -----------------------------------------------------
 
 project = 'skbel'
