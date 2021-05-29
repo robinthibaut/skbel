5aa751a979a986d9888288c7104431d28ea594d8
Robin Thibaut <robin.thibaut@ugent.be>
2021-05-29 16:56:29 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-master_doc = 'index'
+# master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
