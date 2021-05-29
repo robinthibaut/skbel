7a0aba4bd23ea6841d29e0d3ba8d480bb84bd6d0
Robin Thibaut <robin.thibaut@ugent.be>
2021-05-29 17:01:26 +0200

    Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
@@ -22,16 +21,14 @@ project = 'skbel'
 copyright = '2021, Robin Thibaut'
 author = 'Robin Thibaut'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.1'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
