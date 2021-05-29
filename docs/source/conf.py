e72659df10793e65ed3426e07a159050d3a4b457
Robin Thibaut <robin.thibaut@ugent.be>
2021-05-29 16:47:18 +0200

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-master_doc = 'docs/index.rst'
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
