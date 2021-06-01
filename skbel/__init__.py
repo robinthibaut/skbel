3b8534d3ff7a4b46b4a86095f5c613fbf96f4d68
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-01 12:20:28 +0200

    Minor changes

--- skbel/__init__.py
+++ skbel/__init__.py
@@ -1,20 +1,20 @@
 #  Copyright (c) 2021. Robin Thibaut, Ghent University
-import os
-from datetime import date
-from os.path import join as jp
+# import os
+# from datetime import date
+# from os.path import join as jp
+#
+# from loguru import logger
 
-from loguru import logger
-
-__version__ = "1.0.dev0"
+__version__ = "1.0.2"
 
 source = __name__.split(".")[-1]
 # Set up logger
-logger.add(
-    jp(os.getcwd(), "logs", f"{source}_{date.today()}.log"),
-    backtrace=True,
-    diagnose=True,
-    enqueue=True,
-)
+# logger.add(
+#     jp(os.getcwd(), "logs", f"{source}_{date.today()}.log"),
+#     backtrace=True,
+#     diagnose=True,
+#     enqueue=True,
+# )
 
 __all__ = [
     "utils",
