d71845da6498b6214ad1271017a51ffac9ff4b37
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-05 12:40:55 +0200

    Minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,8 @@ author = 'Robin Thibaut'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"
+extensions = ["sphinx.ext.autodoc",
+              "sphinx_rtd_theme",
               ]
 
 autodoc_default_options = {'members': True,
@@ -49,7 +50,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
