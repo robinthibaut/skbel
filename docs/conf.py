cd3f222998992ee925804610aeb1e0dcc7e567a8
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-05 11:39:37 +0200

    Minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
