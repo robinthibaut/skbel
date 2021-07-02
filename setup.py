f57a804f6cde121c84e5e827458082c5576d4ca3
Robin Thibaut <robin.thibaut@ugent.be>
2021-07-02 15:55:47 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
     LONG_DESCRIPTION = f.read()
 setup(
     name="skbel",
-    version="1.1.7",
+    version="1.1.8",
     packages=my_pckg,
     include_package_data=True,
     url="https://github.com/robinthibaut/skbel",
