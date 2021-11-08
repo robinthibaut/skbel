Fixed poor distribution scaling

skbel/algorithms/statistics.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import warnings
 from numpy.random import uniform
-from scipy import ndimage
+from scipy import ndimage, integrate
 from sklearn import preprocessing
 from sklearn.neighbors import KernelDensity
 from sklearn.utils import check_array
@@ -439,25 +439,32 @@ def _conditional_distribution(
     return zi, line
 
 
-def _scale_distribution(post: np.array):
+def _scale_distribution(post: np.array, support: np.array):
     """
     Scale the distribution to have a maximum of 1.
     :param post: Values of the KDE cross-section
-    :return:
+    :param support: Support of the KDE cross-section
+    :return: The scaled distribution
     """
 
     post[np.abs(post) < 1e-8] = 0  # Rule of thumb
 
-    if (
-        post.any()
-    ):  # Deals with the case where 'post' consists of an array filled with 0's
-        min_max_scaler = preprocessing.MinMaxScaler()
-        post_minmax = min_max_scaler.fit_transform(post.reshape(-1, 1)).reshape(-1)
+    # if (
+    #     post.any()
+    # ):  # Deals with the case where 'post' consists of an array filled with 0's
+    #     min_max_scaler = preprocessing.MinMaxScaler()
+    #     post_minmax = min_max_scaler.fit_transform(post.reshape(-1, 1)).reshape(-1)
+    #
+    #     return post_minmax
+    #
+    # else:
+    #     return post
 
-        return post_minmax
+    if post.any():  # Rule of thumb
+        a = integrate.simps(y=np.abs(post), x=support)
+        post *= 1 / a
 
-    else:
-        return post
+    return post
 
 
 def posterior_conditional(
@@ -481,11 +488,10 @@ def posterior_conditional(
 
     if X_obs is not None:
         # Extract the density values along the line, using cubic interpolation
-        if type(X_obs) is list or type(X_obs) is tuple:
-            X_obs = X_obs[0]
         post, line = _conditional_distribution(
-            x=X_obs, x_array=xg, y_array=yg, kde_array=dens, k=k
+            x=X_obs[0], x_array=xg, y_array=yg, kde_array=dens, k=k
         )
+        sup = yg
     elif Y_obs is not None:
         # Extract the density values along the line, using cubic interpolation
         if type(Y_obs) is list or tuple:
@@ -493,13 +499,13 @@ def posterior_conditional(
         post, line = _conditional_distribution(
             y=Y_obs, x_array=xg, y_array=yg, kde_array=dens, k=k
         )
-
+        sup = xg
     else:
         msg = "No observation point included."
         warnings.warn(msg, UserWarning)
         return 0
 
-    post = _scale_distribution(post)
+    post = _scale_distribution(post, sup)
 
     return post, line
 

skbel/goggles/visualization.py
@@ -629,7 +629,7 @@ def _kde_cca(
     """
     Plot the kernel density estimate of the CCA.
     :param bel: The BEL object.
-    :param obs_n: The number of observations to use.
+    :param obs_n: The index of the observation to plot.
     :param X_obs: The X observations.
     :param Y_obs: The Y observations.
     :param sdir: The directory to save the plot to.
@@ -657,7 +657,9 @@ def _kde_cca(
     # Transform X obs, Y obs
     X_obs_f, Y_obs_f = bel.transform(X=X_obs, Y=Y_obs)  # Transform X obs, Y obs
 
-    samples = bel.random_sample(X_obs_f=X_obs_f, obs_n=obs_n, n_posts=100)  # Get 100 samples
+    samples = bel.random_sample(
+        X_obs_f=X_obs_f, obs_n=obs_n, n_posts=100
+    )  # Get 100 samples
 
     for comp_n in range(bel.cca.n_components):
         # Get figure default parameters
@@ -680,7 +682,7 @@ def _kde_cca(
 
             # Conditional:
             hp, sup = posterior_conditional(
-                X_obs=X_obs_f[obs_n].T[comp_n], dens=density, support=support, k=200
+                X_obs=X_obs_f.T[comp_n], dens=density, support=support, k=200
             )  # Get posterior
 
             # Filled contour plot
@@ -705,7 +707,7 @@ def _kde_cca(
             pass
         # Vertical line
         ax_joint.axvline(
-            x=X_obs_f[obs_n].T[comp_n],
+            x=X_obs_f.T[comp_n],
             color="red",
             linewidth=1,
             alpha=0.5,
@@ -713,7 +715,7 @@ def _kde_cca(
         )
         # Horizontal line
         ax_joint.axhline(
-            y=Y_obs_f[obs_n].T[comp_n],
+            y=Y_obs_f.T[comp_n],
             color="deepskyblue",
             linewidth=1,
             alpha=0.5,
@@ -730,15 +732,15 @@ def _kde_cca(
             alpha=0.9,
         )
         ax_joint.plot(
-            np.ones(samples.shape[1]) * X_obs_f[obs_n].T[comp_n],
-            samples[obs_n].T[comp_n],
+            np.ones(samples.shape[1]) * X_obs_f.T[comp_n],
+            samples.T[comp_n],
             "go",
             alpha=0.3,
-        )
+        )  # Samples
         # Point
         ax_joint.plot(
-            bel.X_obs_f[obs_n].T[comp_n],
-            bel.Y_obs_f[obs_n].T[comp_n],
+            X_obs_f.T[comp_n],
+            Y_obs_f.T[comp_n],
             "wo",
             markersize=5,
             markeredgecolor="k",
@@ -766,7 +768,7 @@ def _kde_cca(
         )
         #  - Notch indicating true value
         ax_marg_y.axhline(
-            y=bel.Y_obs_f.T[comp_n],
+            y=Y_obs_f.T[comp_n],
             xmax=0.25,
             color="deepskyblue",
             linewidth=1,